bf87d034ef15e024fa090c98a055f43733669bc9
paulaji <paulajiparayil123@gmail.com>
2023-06-26 11:17:52 +0530

    morgan has tiny, combined etc

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 const app = express();
 
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 6969;
+
+// logging requests using morgan
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("this is a CRUD app");
